d116f1b21a6161ea62ceed8936270b3403cfd353
Vincenzo Calabretta <vincenzo.cal.main@gmail.com>
2026-08-08 23:18:11 +0200

    Update consumer pins after documentation history scrub

--- tests/consumer/flake.nix
+++ tests/consumer/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     # Keep this revision identical to the Bazel git_override in a published
     # consumer. Repository tests override this input with the local checkout.
-    zub_1cg.url = "git+ssh://git@10.10.0.101/v/zub_1cg.git?rev=85a35fdb7edaf79f67dadb1252dcbbf636bea588";
+    zub_1cg.url = "git+ssh://git@10.10.0.101/v/zub_1cg.git?rev=c41bf3e49c9ead7eab127653bb8938d7609538e1";
   };
   outputs = { zub_1cg, ... }: {
     devShells.x86_64-linux.default = zub_1cg.lib.mkDevShell {
